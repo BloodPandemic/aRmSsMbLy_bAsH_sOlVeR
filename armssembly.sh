a39de78765f1d75cb9b3bc18d9402fa7e90c71f5
NullBytes <119623220+BloodPandemic@users.noreply.github.com>
2023-08-25 10:10:35 -0700

    Update armssembly.sh
    some changes on the script. Prolly made it a bit more reliable and , yeah pretty much it!

--- armssembly.sh
+++ armssembly.sh
@@ -1,39 +1,44 @@
-#!/bin/bash
-
-read -p "Enter the url: " url
+#!/bin/bash 
+read -p "Enter the url: " url 
 wget "${url}"
-read -p "Enter the number the challenge gives: " argument
+read -p "Enter the number the challenge gives: " argument 
 
 OUT="chall.o"
-read -p "ARMssembly? {nr}: " num
-if [ $num -eq 0 ]; 
-then
+read -p "ARMssembly? {nr}: " num 
+if [ $num -eq 0 ];  then 
 	file="chall.S"
-else
+else 
 	file="chall_${num}.S"
 fi
-
-while true; do
-	    if [ ! -e "$OUT" ]; then
-		    OUT2="chall${num}_compiled"
-		    aarch64-linux-gnu-as -o "${OUT}" "${file}"
-		    sleep 1
-		    aarch64-linux-gnu-gcc "${OUT}" -static -o "${OUT2}"
-		    echo "[+] Asm was compiled to a binary ^==^"
-		    break
-	    else
-                    read -p "[!] A file named ${OUT} is already present. Do you wish to remove it so we can proceed (y/n): " choice
-	            if [ "$choice" == "y" ]; then
-			rm "${OUT}"
+OUT2="chall${num}_compiled"
+while true; do 
+	if [ ! -e "$OUT" ] || [ ! -e "${OUT2}" ]; then 
+		aarch64-linux-gnu-as -o "${OUT}" "${file}"
+		sleep 1 
+		aarch64-linux-gnu-gcc "${OUT}" -static -o "${OUT2}"
+		echo "[+] Asm was compiled to a binary ^==^"
+		break 
+	else 
+		read -p "[!] A file named ${OUT}/${OUT2} is already present. Do you wish to remove it so we can proceed (y/n): " choice 
+		if [ "$choice"=="y"]; then 
+			if [ ! -f ${OUT}]; then 
+				rm "${OUT2}"
+			elif [ ! -f ${OUT2}]; then 
+				rm "${OUT2}"
+			else 
+				rm "${OUT}" "${OUT2}"
+			fi 
 			continue
-		    else
+       		else 
 			echo "[-] Closing the script..."
-			exit 1	
-		        fi														        fi
-done
-
-															echo "[+] Running the binary..."
+			exit 1 
+			
+		fi 
+	fi 
+done 
+echo "[+] Running the binary..."
 COM="$(sudo qemu-aarch64-static "${OUT2}" "${argument}" -release)"
 RES="${COM##Result: }"
 echo "[+] Result is ..."
-printf "%x\n" "${RES}"
+printf "picoCTF{%x}\n" "${RES}"
+rm "${OUT}" "${file}" "${OUT2}"
